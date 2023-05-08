cdb7281ee54e3f5143fc0bc30ee142da34ae4362
abhircb5540 <131947409+abhircb5540@users.noreply.github.com>
2023-05-08 11:54:39 +0530

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,14 @@
+def selctionsort(a):
+    n = len(a)
+    for i in range(n-2):
+        min=i
+        for j in range(i+1,n-1):
+            if a[j]<a[min]:
+                min=j
+                temp=a[i]
+                a[i] = a[min]
+                a[min] = temp
+                x =[34,46,43,27,57,41,45,21,70]
+                print("before sorting:",x)
+                selectionsort(x)
+                print("after sorting:",x)
